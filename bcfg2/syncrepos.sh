de41d9ff162625d3b4bc8dcf31f0912a50a3eafc
Jack Neely <jjneely@ncsu.edu>
2009-11-12 17:46:48 -0500

    Bcfg2 repository maintenance scripts.
    The Makefile does some maintenance functions on the repos that live out
    in AFS.
    
    syncrepos.sh runs on the Bcfg2 servers to find and apply all changes.

--- bcfg2/syncrepos.sh
+++ bcfg2/syncrepos.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+# Run this script in cron with stdout redirected to /dev/null
+# Important errors are written to stderr
+
 MASTERREPOS=/afs/bp/system/config/linux-kickstart/bcfg2
-LOCALREPOS=/tmp/bcfg2repos
+LOCALREPOS=/srv/bcfg2
 
 if [ ! -x /usr/bin/git ] ; then
     echo "Git not installed.  Bailing."
@@ -13,14 +16,41 @@ if [ ! -d $LOCALREPOS ] ; then
 fi
 
 for r in `ls $MASTERREPOS`; do
+    if [ ! -d $MASTERREPOS/$r ] ; then continue ; fi
     if [ -d $LOCALREPOS/$r ] ; then
         cd $LOCALREPOS/$r
         git pull
     elif [ -e $LOCALREPOS/$r ] ; then
-        echo "ERROR: Non-directory $LOCALREPOS/$r already exists..."
+        echo "ERROR: Non-directory $LOCALREPOS/$r already exists..." > /dev/stderr
+        continue
     else
         cd $LOCALREPOS
         git clone $MASTERREPOS/$r $r
+    fi
+
+    if [ -e $LOCALREPOS/$r/bcfg2.pid ] ; then
+        PID=`cat $LOCALREPOS/$r/bcfg2.pid`
+    else
+        PID="X"
+    fi
+
+    if [ "$PID" != "X" -a -e /proc/$PID ] ; then
+        # We are done...bcfg2-server is running
+        continue
+    fi
+
+    if [ ! -e $LOCALREPOS/$r/bcfg2.conf ] ; then
+        # No config file
+        echo "$LOCALREPOS/$r lacks bcfg2.conf" > /dev/stderr
+        continue
+    fi
+
+    # Start up a bcfg2 server for this repo
+    echo "Starting Bcfg2 server for $LOCALREPOS/$r"
+    /usr/sbin/bcfg2-server -D $LOCALREPOS/$r/bcfg2.pid \
+                           -C $LOCALREPOS/$r/bcfg2.conf
+    if [ "$?" != "0" ] ; then
+        echo "ERROR: Failed to start bcfg2-server for $LOCALREPOS/$r" > /dev/stderr
     fi
 done
 
