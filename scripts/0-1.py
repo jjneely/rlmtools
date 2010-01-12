2798f1ac5b671e2b450844f9198971d3d48f9b94
Jack Neely <jjneely@ncsu.edu>
2010-01-12 14:13:23 -0500

    This script aids in DB migration from the old schema.

---
+++ scripts/0-1.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+
+# A python script to help in the db migration process during the 
+# testing phase
+
+from rlmtools.adminServer import AdminServer
+from rlmtools.constants import defaultConfFiles
+from rlmtools import configDragon
+
+configDragon.initConfig(defaultConfFiles)
+
+s = AdminServer()
+s._setupParents()
+
+root = s.getDeptID('root')
+for row in s.getPTSGroups():
+    if row['name'] == 'admin':
+        s.setPerm(row['acl_id'], root, 7)
+    if row['name'] == 'installer:common':
+        s.setPerm(row['acl_id'], root, 4)
+
