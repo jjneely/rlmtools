81d15fac68f0b7d930d006a8b993d38b7e6c9d95
Jack Neely <jjneely@ncsu.edu>
2012-05-10 16:14:07 -0400

    a puppet external node classifier
    This is what an puppet ENC might look like

---
+++ puppet/enc.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+
+import re
+import sys
+import yaml
+import optparse
+import logging
+import logging.handlers
+
+from rlmtools import rlattributes
+from rlmtools import apiServer
+from rlmtools import configDragon
+from rlmtools.constants import defaultConfFiles
+
+# This matches our "fqdn-UUID" cert naming scheme
+regex = "([-_a-zA-Z0-9.]+)-([a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{12})"
+
+def getParams(hostname, uuid):
+    RLA = rlattributes.RLAttributes()
+    api = apiServer.APIServer(2, hostname, uuid)
+    host_id = api.getUuidID(uuid)
+    if host_id is None:
+        # Unregistered host
+        return {}
+
+    m, a = RLA.hostAttrs(host_id)
+    a["meta"] = m
+    return { "rlmtools": a }
+
+def main():
+    parser = optparse.OptionParser()
+    parser.add_option("-C", "--configfile", action="store",
+            default=defaultConfFiles,
+            dest="configfile",
+            help="Configuration file")
+    (options, args) = parser.parse_args()
+
+    # Start up configuration/logging/databases of RLMTools
+    configDragon.initConfig(options.configfile)
+    log = logging.getLogger("xmlrpc.enc")
+
+    argc = len(args)
+
+    if argc < 1:
+        log.error("ENC called without host/certname as first argument")
+        sys.exit(1)
+
+    certname = args[0]
+    match = re.match(regex, certname)
+    if match is not None:
+        hostname = match.group(1)
+        uuid = match.group(2)
+        log.info("Generating YAML for puppet with RLMTools attributes for "
+                 "host %s and UUID: %s" % (hostname, uuid))
+    else:
+        log.error("Could not parse certname into host and UUID pair.")
+        # A non-realm-linux box?  Sure, we'll take these...might as well
+        sys.exit(0)
+
+    doc = {"parameters": getParams(hostname, uuid), }
+
+    print yaml.dump(doc)
+
+
+if __name__ == "__main__":
+    main()
+
