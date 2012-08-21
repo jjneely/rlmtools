bdb0ff58ef68ea1d46013bbce71cf4341129f322
Jack Neely <jjneely@ncsu.edu>
2012-08-21 18:23:43 -0400

    puppet API: Signing certs now works
    A NOTES file was added to document some of the permissions bits needed
    on the RLMTools server as they become Puppet Clients themselves.

--- rlmtools/puppet.py
+++ rlmtools/puppet.py
@@ -15,23 +15,24 @@ class Puppet(object):
         ssldir = "/var/lib/puppet/ssl"
 
         self.ca = os.path.join(ssldir, "certs/ca.pem")
-        if not os.path.exists(ca):
+        if not os.path.exists(self.ca):
             raise StandardError("Puppet CA certificate not found: %s" % ca)
 
         uuid = self.getLocalUUID()
         fqdn = gethostname()
 
-        self.certname = "%s-%s" % (fqnd, uuid)
+        self.certname = "%s-%s" % (fqdn, uuid)
+        pem = self.certname + ".pem"
 
-        self.key  = os.path.join(ssldir, "private_keys/", self.certname)
-        self.cert = os.path.join(ssldir, "certs/", self.certname)
+        self.key  = os.path.join(ssldir, "private_keys/", pem)
+        self.cert = os.path.join(ssldir, "certs/", pem)
 
         for i in [self.key, self.cert]:
             if not os.path.exists(i):
                 msg = "Puppet Certificate/Key not found: %s" % i
                 raise StandardError(msg)
 
-        self.http = restclient.RestClient(url, ca=self.ca, sslkey=self.key
+        self.http = restclient.RestClient(url, ca=self.ca, sslkey=self.key,
                 sslcrt=self.cert)
 
     def getLocalUUID(self):
@@ -45,5 +46,50 @@ class Puppet(object):
 
         return blob.strip()
 
-    
+    def getCertStatus(self, fqdn, uuid):
+        client = "%s-%s" % (fqdn, uuid)
+        r, c = self.http.get("/root/certificate_status/%s" % client,
+                headers = {'Accept':'pson'})
 
+        if r.status == 404:
+            # There is no certificate
+            return None
+        elif r.status != 200:
+            # We got an error back
+            log.error("Puppet API Error: %s - %s" % (r.reason, c))
+            raise StandardError("Puppet API Error: %s - %s" % (r.reason, c))
+
+        # Return the dict from Puppet: keys: name, state, fingerprint
+        return json.loads(c)
+
+    def signCert(self, fqdn, uuid, fingerprint):
+        meta = self.getCertStatus(fqdn, uuid)
+        if meta == None:
+            log.warning("Puppet API: Asked to sign non-existant cert for %s" \
+                    % uuid)
+            return False
+
+        if meta['fingerprint'] != fingerprint:
+            log.warning("Puppet API: Cert fingerprint missmatch for %s" \
+                    % uuid)
+            return False
+
+        if meta['state'].lower() == "signed":
+            log.info("Puppet API: Cert for %s is already signed" % uuid)
+            return True
+
+        r, c = self.http.put("/root/certificate_status/%s-%s" \
+                % (fqdn, uuid), 
+                headers = {'Accept': 'pson',
+                           'Content-Type': 'text/pson'},
+                body = json.dumps({"desired_state":"signed"}))
+
+        # We shouldn't get a 404 here, we just poked it
+        if r.status != 200:
+            log.error("Puppet API: Error signing cert for %s: %s - %s" \
+                    % (uuid, r.reason, c))
+            return False
+
+        # Request should now be signed
+        return True
+    
