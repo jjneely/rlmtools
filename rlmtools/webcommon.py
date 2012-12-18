2b22f0a17328f2f1e4f4f3bddaa9f63d9a325014
Jack Neely <jjneely@ncsu.edu>
2012-12-18 14:42:27 -0500

    Re-arranve webcommon.py
    All of the auth related stuff is together.  Helper functions together.
    Code that runs before each and before the first request are next to each
    other.  You know...friendly things.

--- rlmtools/webcommon.py
+++ rlmtools/webcommon.py
@@ -15,15 +15,6 @@ from rlmtools import app
 
 log = logging.getLogger('xmlrpc')
 
-def url():
-    base = request.url_root
-    if base.endswith('/'):
-        return base[:-1]
-    else:
-        return base
-
-def short(fqdn): return fqdn.split('.')[0]
-
 class Auth(object):
    
     # XXX: Cache location of WRAP key?
@@ -71,23 +62,6 @@ class Auth(object):
             return "Guest User"
         return pwd.getpwnam(self.userid)[4]        
 
-# Permission bitfield operations
-ADMIN = 0x01
-WRITE = 0x02
-READ  = 0x04
-
-def isADMIN(field): return (field & ADMIN) == 1
-def isWRITE(field): return (field & WRITE) >> 1 == 1
-def isREAD(field): return (field & READ) >> 2 == 1
-
-def adminOf(dept_id): return isADMIN(getAuthZ(dept_id))
-
-def mapPermBits(field):
-    if isADMIN(field): return "admin"
-    if isWRITE(field): return "write"
-    if isREAD(field) : return "read"
-    return "unknown"
-
 _tloader = TemplateLoader([os.path.join(os.path.dirname(__file__),
                           'templates')], auto_reload=True)
 _server = None
@@ -124,6 +98,53 @@ app.before_first_request(_init_webcommon)
 app.before_request(_before_each_fqdn)
 app.before_request(_before_each_auth)
 
+# Permission bitfield operations
+ADMIN = 0x01
+WRITE = 0x02
+READ  = 0x04
+
+def isADMIN(field): return (field & ADMIN) == 1
+def isWRITE(field): return (field & WRITE) >> 1 == 1
+def isREAD(field): return (field & READ) >> 2 == 1
+
+def adminOf(dept_id): return isADMIN(getAuthZ(dept_id))
+
+def mapPermBits(field):
+    if isADMIN(field): return "admin"
+    if isWRITE(field): return "write"
+    if isREAD(field) : return "read"
+    return "unknown"
+
+def isAuthenticated():
+    return g.auth.isAuthenticated()
+
+def getAuthZ(dept):
+    """Return the bitfield that indicates what access permissions the user
+       has that is currently poking the web interface.  0 Mean no rights."""
+
+    # Basic sanity
+    if not g.auth.isAuthenticated():
+        return 0
+
+    # Check the default initial admin
+    if g.auth.userid == configDragon.config.default_admin:
+        return 7   # read, write, admin
+
+    # What is our dept_id?
+    d = _parseDept(dept)
+
+    perm = _server.getAccess(g.auth.userid, d)
+    return perm
+
+def url():
+    base = request.url_root
+    if base.endswith('/'):
+        return base[:-1]
+    else:
+        return base
+
+def short(fqdn): return fqdn.split('.')[0]
+
 def render(tmpl, dict):
     # Add some default variables
     dict['name'] = g.auth.getName()
@@ -148,9 +169,6 @@ def message(s):
                            fullname=g.auth.getName(),
                       ))
 
-def isAuthenticated():
-    return g.auth.isAuthenticated()
-
 def _parseDept(void):
     """Figure out what the ID of the dept is from whatever is in void.
        Returns an int or None."""
@@ -160,24 +178,6 @@ def _parseDept(void):
         return int(void)
     # If we are here this must be a name, use the DB to convert
     return self._server.getDeptIDNoCreate(void)
-
-def getAuthZ(dept):
-    """Return the bitfield that indicates what access permissions the user
-       has that is currently poking the web interface.  0 Mean no rights."""
-
-    # Basic sanity
-    if not g.auth.isAuthenticated():
-        return 0
-
-    # Check the default initial admin
-    if g.auth.userid == configDragon.config.default_admin:
-        return 7   # read, write, admin
-
-    # What is our dept_id?
-    d = self._parseDept(dept)
-
-    perm = self._server.getAccess(g.auth.userid, d)
-    return perm
 
 @app.errorhandler(401)
 def error401(error):
